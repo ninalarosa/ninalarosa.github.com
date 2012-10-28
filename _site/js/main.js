72ad2ab00c741df5008fd5413bb5657217b6453b
Patrick Berkeley <patrickberkeley@gmail.com>
2012-10-28 11:52:56 -0400

    Correct when background opacity is set.

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,23 +1,33 @@
-$(function() {
+ndl = {};
+
+ndl.init = function() {
   var $header = $('header'),
       $headerShadow = $('.header-shadow'),
       boxes = $('.alpha'),
       headerHeight = $header.outerHeight();
-      headerBackground = jQuery.Color($header.css('backgroundColor'));
+      headerBackground = jQuery.Color($header.css('backgroundColor')),
+      headerAlpha = headerBackground.alpha();
 
   $headerShadow.css('opacity', 0);
 
   $(document).scroll(function(e){
     var scrollPercent = (window.scrollY / headerHeight),
-        boxesAlpha = (headerBackground.alpha() + ((1 - headerBackground.alpha()) * (window.scrollY / headerHeight))).toFixed(2),
+        boxesAlpha = (headerAlpha + ((1 - headerAlpha) * (window.scrollY / headerHeight))).toFixed(2),
         headerShadowAlpha = (1 * (window.scrollY / (3 * headerHeight))).toFixed(2);
 
+    // Change the header's shadow opacity based on the amount the page has been
+    // scrolled.
     if (scrollPercent >= 0 && scrollPercent < 3) {
       $headerShadow.css('opacity', headerShadowAlpha);
     }
-    if (scrollPercent >= 0 && scrollPercent < 1) {
+    // Change the .alpha element's backgrounds based until they're opaque.
+    if (headerAlpha >= 0 && headerAlpha < 1) {
       boxes.css('backgroundColor', headerBackground.alpha(boxesAlpha) );
     }
   });
+};
+
+$(function() {
+  ndl.init();
 });
 
