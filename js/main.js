f558c81ca38fed7e421835cb1b740b124f2681d5
Patrick Berkeley <patrickberkeley@gmail.com>
2012-10-28 02:19:33 -0400

    Add fancy drop shadow to separate header from page content.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 $(function() {
-  var $el = $('.alpha'),
-      elHeight = $el.outerHeight();
-      elBackground = jQuery.Color($el.css('backgroundColor'));
+  var $header = $('header'),
+      $headerShadow = $('.header-shadow'),
+      boxes = $('.alpha'),
+      headerHeight = $header.outerHeight();
+      headerBackground = jQuery.Color($header.css('backgroundColor'));
+
+  $headerShadow.css('opacity', 0);
 
   $(document).scroll(function(e){
-    var scrollPercent = (elHeight - window.scrollY) / elHeight,
-        alpha = (elBackground.alpha() + ((1 - elBackground.alpha()) * (window.scrollY / elHeight))).toFixed(2);
+    var scrollPercent = (window.scrollY / headerHeight),
+        boxesAlpha = (headerBackground.alpha() + ((1 - headerBackground.alpha()) * (window.scrollY / headerHeight))).toFixed(2),
+        headerShadowAlpha = (1 * (window.scrollY / (3 * headerHeight))).toFixed(2);
 
+    if (scrollPercent >= 0 && scrollPercent < 3) {
+      $headerShadow.css('opacity', headerShadowAlpha);
+    }
     if (scrollPercent >= 0 && scrollPercent < 1) {
-      $el.css('backgroundColor', elBackground.alpha(alpha) );
+      boxes.css('backgroundColor', headerBackground.alpha(boxesAlpha) );
     }
   });
 });
