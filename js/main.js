f6b356da18441a967a73a929bdfbc53da29fff15
Patrick Berkeley <patrickberkeley@gmail.com>
2012-10-28 01:15:51 -0400

    Fix header to the top and make it fade in on scroll.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(function() {
+  var $el = $('.alpha'),
+      elHeight = $el.outerHeight();
+      elBackground = jQuery.Color($el.css('backgroundColor'));
+
+  $(document).scroll(function(e){
+    var scrollPercent = (elHeight - window.scrollY) / elHeight,
+        alpha = (elBackground.alpha() + ((1 - elBackground.alpha()) * (window.scrollY / elHeight))).toFixed(2);
+
+    if (scrollPercent >= 0 && scrollPercent < 1) {
+      $el.css('backgroundColor', elBackground.alpha(alpha) );
+    }
+  });
+});
 
